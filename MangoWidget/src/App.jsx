7195dd4ab755551e65cad066f687ba74dec54c26
Angel Robles <mangodev03@gmail.com>
2025-03-01 15:07:00 +0100

    Fixed some font issues

--- MangoWidget/src/App.jsx
+++ MangoWidget/src/App.jsx
@@ -3,16 +3,12 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useNavigate,
 } from "react-router-dom";
 import SpotifyWebApi from "spotify-web-api-js";
 import "./App.css";
+import LoggedIn from "./LoggedIn";
 
 const spotifyApi = new SpotifyWebApi();
-const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
-const RESPONSE = "token";
-const SCOPE = "user-read-currently-playing";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -49,7 +45,7 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<LoggedIn loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
+            element={<LoggedIn spotifyApi={spotifyApi} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
           />
         </Routes>
       </Router>
@@ -57,133 +53,6 @@ function App() {
   );
 }
 
-function LoggedIn({ loggedIn, setLoggedIn }) {
-  const [currentSong, setCurrentSong] = useState({
-    name: "",
-    artist: "",
-    albumImage: "",
-  });
 
-  const navigate = useNavigate();
-  const nameRef = useRef(null);
-  const artistRef = useRef(null);
-
-  useEffect(() => {
-    if (loggedIn) {
-      getCurrentSong();
-      const interval = setInterval(() => {
-        getCurrentSong();
-      }, 3000);
-      return () => clearInterval(interval);
-    }
-  }, [loggedIn]);
-
-  useEffect(() => {
-    const nameElement = nameRef.current;
-    const artistElement = artistRef.current;
-    if (nameElement && artistElement) {
-      const nameOverflow = nameElement.scrollWidth > nameElement.clientWidth;
-      const artistOverflow =
-        artistElement.scrollWidth > artistElement.clientWidth;
-
-      if (nameOverflow) {
-        nameElement.classList.add("slide-in");
-      } else {
-        nameElement.classList.remove("slide-in");
-      }
-
-      if (artistOverflow) {
-        artistElement.classList.add("slide-in");
-      } else {
-        artistElement.classList.remove("slide-in");
-      }
-    }
-  }, [currentSong]);
-
-  function getCurrentSong() {
-    spotifyApi
-      .getMyCurrentPlayingTrack()
-      .then((response) => {
-        console.log(response);
-        if (response && response.item) {
-          setCurrentSong({
-            name: response.item.name,
-            artist: response.item.artists[0].name,
-            albumImage: response.item.album.images[0].url,
-          });
-        } else {
-          setCurrentSong({
-            name: "No song currently playing.",
-            artist: "",
-            albumImage: "src/assets/MangoMelodyLogo.jpeg",
-          });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  function handleLoginWithSpotify() {
-    window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=${RESPONSE}`;
-  }
-
-  function handleLogout() {
-    spotifyApi.setAccessToken(null);
-    const width = 700;
-    const height = 500;
-    const left = window.screen.width / 2 - width / 2;
-    const top = window.screen.height / 2 - height / 2;
-    const spotifyLogoutWindow = window.open(
-      "https://www.spotify.com/logout/",
-      "Spotify Logout",
-      `width=${width},height=${height},top=${top},left=${left}`
-    );
-    setTimeout(() => {
-      spotifyLogoutWindow.close();
-      setLoggedIn(false);
-      navigate("/");
-    }, 2000);
-  }
-
-  return (
-    <>
-      {!loggedIn ? (
-        <button onClick={handleLoginWithSpotify}>Login with Spotify</button>
-      ) : (
-        <div className="container-div">
-          <div className="main-div">
-            <div className="img-div">
-              {currentSong.albumImage && (
-                <img src={currentSong.albumImage} alt="Album Cover" />
-              )}
-            </div>
-            <div
-              className="info-background"
-              style={{
-                backgroundImage: `url(${currentSong.albumImage})`,
-              }}
-            />
-            <div className="info-div">
-              <p ref={nameRef} className="name-p">
-                {currentSong.name}
-              </p>
-              <p ref={artistRef} className="artist-p">
-                {currentSong.artist}
-              </p>
-            </div>
-            <div className="arrow-div-container">
-                <div className="arrow-div"></div>
-              </div>
-            <div className="loader"></div>
-          </div>
-          <div className="button-div">
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-        </div>
-      )}
-    </>
-  );
-}
 
 export default App;
---
+++ MangoWidget/src/LoggedIn.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect, useRef } from "react";
+import {
+  useNavigate,
+} from "react-router-dom";
+import "./App.css";
+
+function LoggedIn({ spotifyApi, loggedIn, setLoggedIn }) {
+    const [currentSong, setCurrentSong] = useState({
+      name: "",
+      artist: "",
+      albumImage: "",
+    });
+
+    const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+    const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+    const RESPONSE = "token";
+    const SCOPE = "user-read-currently-playing";
+  
+    const navigate = useNavigate();
+    const nameRef = useRef(null);
+    const artistRef = useRef(null);
+  
+    useEffect(() => {
+      if (loggedIn) {
+        getCurrentSong();
+        const interval = setInterval(() => {
+          getCurrentSong();
+        }, 3000);
+        return () => clearInterval(interval);
+      }
+    }, [loggedIn]);
+  
+    useEffect(() => {
+      const nameElement = nameRef.current;
+      const artistElement = artistRef.current;
+      if (nameElement && artistElement) {
+        const nameOverflow = nameElement.scrollWidth > nameElement.clientWidth;
+        const artistOverflow =
+          artistElement.scrollWidth > artistElement.clientWidth;
+  
+        if (nameOverflow) {
+          nameElement.classList.add("slide-in");
+        } else {
+          nameElement.classList.remove("slide-in");
+        }
+  
+        if (artistOverflow) {
+          artistElement.classList.add("slide-in");
+        } else {
+          artistElement.classList.remove("slide-in");
+        }
+      }
+    }, [currentSong]);
+  
+    function getCurrentSong() {
+      spotifyApi
+        .getMyCurrentPlayingTrack()
+        .then((response) => {
+          console.log(response);
+          if (response && response.item) {
+            setCurrentSong({
+              name: response.item.name,
+              artist: response.item.artists[0].name,
+              albumImage: response.item.album.images[0].url,
+            });
+          } else {
+            setCurrentSong({
+              name: "No song currently playing.",
+              artist: "",
+              albumImage: "src/assets/MangoMelodyLogo.jpeg",
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  
+    function handleLoginWithSpotify() {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=${RESPONSE}`;
+    }
+  
+    function handleLogout() {
+      spotifyApi.setAccessToken(null);
+      const width = 700;
+      const height = 500;
+      const left = window.screen.width / 2 - width / 2;
+      const top = window.screen.height / 2 - height / 2;
+      const spotifyLogoutWindow = window.open(
+        "https://www.spotify.com/logout/",
+        "Spotify Logout",
+        `width=${width},height=${height},top=${top},left=${left}`
+      );
+      setTimeout(() => {
+        spotifyLogoutWindow.close();
+        setLoggedIn(false);
+        navigate("/");
+      }, 2000);
+    }
+  
+    return (
+      <>
+        {!loggedIn ? (
+          <button onClick={handleLoginWithSpotify}>Login with Spotify</button>
+        ) : (
+          <div className="container-div">
+            <div className="main-div">
+              <div className="img-div">
+                {currentSong.albumImage && (
+                  <img src={currentSong.albumImage} alt="Album Cover" />
+                )}
+              </div>
+              <div
+                className="info-background"
+                style={{
+                  backgroundImage: `url(${currentSong.albumImage})`,
+                }}
+              />
+              <div className="info-div">
+                <p ref={nameRef} className="name-p">
+                  {currentSong.name}
+                </p>
+                <p ref={artistRef} className="artist-p">
+                  {currentSong.artist}
+                </p>
+              </div>
+              {/* <div className="arrow-div-container">
+                  <div className="arrow-div"></div>
+            </div> */}
+              <div className="loader"></div>
+            </div>
+            <div className="button-div">
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
+
+  export default LoggedIn;
